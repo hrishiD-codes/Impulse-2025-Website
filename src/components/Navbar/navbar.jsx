a313d3996ed5a5d16e02a9f089c884aa84639d2e
HRISHIKESH DAS <dashrishikesh900@gmail.com>
2025-08-06 00:39:41 +0530

    Feat: Navbar and Cards added
    Co-Authored-By: Subhro Mohanta <117219000+imSubhro@users.noreply.github.com>

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,11 +1,14 @@
 
 import '../../index.css';
 import { useState } from 'react';
-import { IoIosMenu, IoMdHome } from "react-icons/io";
-import { FaFacebookF, FaInstagram, FaYoutube } from 'react-icons/fa';
+import Star from './star.jsx';
+import facebook from '../../../public/icons/facebook.svg';
+import instagram from '../../../public/icons/instagram.svg';
+import youtube from '../../../public/icons/youtube.svg';
 
 export default function Navbar() {
     const [isSidenavOpen, setIsSidenavOpen] = useState(false);
+    const [isHovered, setIsHovered] = useState(false);
 
     const openNav = () => {
         setIsSidenavOpen(true);
@@ -17,69 +20,103 @@ export default function Navbar() {
 
     return (
         <nav>
-            {/* Sidenav */}
-            <div className={`fixed top-0 left-0 h-full bg-black z-50 overflow-x-hidden transition-all duration-500 pt-16 text-center ${
-                isSidenavOpen ? 'w-full' : 'w-0'
-            }`}>
-                <button 
-                    className="h-8 w-8 border border-white rounded-full flex justify-center items-center hover:bg-white hover:text-black text-white transition-colors cursor-pointer absolute top-6 right-6"
-                    onClick={closeNav}
-                    aria-label="Close navigation"
-                >
-                    <IoMdHome className="h-5 w-5" />
-                </button>
+            <div className='cursor-pointer'
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+                onClick={openNav}>
 
-                {/* Navigation links */}
-                <div className="flex flex-col items-center justify-center gap-6 mt-8">
-                    <a href="#about" className="block text-2xl text-gray-400 hover:text-white transition-colors duration-300 no-underline">
-                        About Impulse
+                <div className=" absolute top-0 left-0 h-15 w-15 z-10 text-orange-400 mt-4 ml-4 hover:rotate-180 transition-all duration-500">
+                    <Star />
+                </div>
+                <h1 className='absolute top-6.5 left-11 text-3xl z-15 font-extrabold text-white'>
+                    MENU
+                </h1>
+
+                <div className={`hidden md:block absolute top-5 left-4 h-[30rem] w-[18rem] bg-[url(../../../public/giphy.gif)] rounded-lg z-5 transition-all duration-300 ${isHovered ? 'opacity-100 visible' : 'opacity-0 invisible'
+                    }`}>
+                    <div className="flex flex-col p-4 gap-6 justify-center items-start mt-15 pl-7">
+                        <a href="/" className="block text-2xl bg-white rounded-tl-2xl rounded-br-2xl text-black hover:scale-120 transition-all duration-300 no-underline whitespace-nowrap px-6 py-1 font-medium">
+                            HOME
+                        </a>
+                        <a href="/#about" className="block text-2xl bg-white rounded-tl-2xl rounded-br-2xl text-black hover:scale-120 transition-all duration-300 no-underline whitespace-nowrap px-6 py-1 font-medium">
+                            ABOUT
+                        </a>
+                        <a href="/#shades" className="block text-2xl bg-white rounded-tl-2xl rounded-br-2xl text-black hover:scale-120 transition-all duration-300 no-underline whitespace-nowrap px-6 py-1 font-medium">
+                            ARTISTS
+                        </a>
+                        <a href="/#events" className="block text-2xl bg-white rounded-tl-2xl rounded-br-2xl text-black hover:scale-120 transition-all duration-300 no-underline whitespace-nowrap px-6 py-1 font-medium">
+                            EVENTS
+                        </a>
+                        <a href="/teams" className="block text-2xl bg-white rounded-tl-2xl rounded-br-2xl text-black hover:scale-120 transition-all duration-300 no-underline whitespace-nowrap px-6 py-1 font-medium">
+                            TEAM
+                        </a>
+                        <a href="/contact" className="block text-2xl bg-white rounded-tl-2xl rounded-br-2xl text-black hover:scale-120 transition-all duration-300 no-underline whitespace-nowrap px-6 py-1 font-medium">
+                            GET IN TOUCH
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div className={`md:hidden fixed top-0 left-0 h-screen w-screen bg-[url(../../../public/giphy.gif)] bg-cover bg-center bg-no-repeat bg-fixed z-50 overflow-x-hidden transition-all duration-500 pt-16 text-center ${isSidenavOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+                <div className='cursor-pointer z-2' onClick={closeNav}
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                >
+                    <div className="absolute top-0 left-0 h-15 w-15 text-orange-400 mt-4 ml-4 hover:rotate-180 transition-all duration-500">
+                        <Star />
+                    </div>
+                    <h1 className='absolute top-6.5 left-11 text-3xl font-extrabold text-white'>
+                        {isSidenavOpen ? 'CLOSE' : 'MENU'}
+                    </h1>
+                </div>
+
+                <div className="flex flex-col items-center justify-center gap-6 mt-15">
+                    <a href="/" className="block text-2xl text-white no-underline font-bold">
+                        HOME
                     </a>
-                    <a href="#events" className="block text-2xl text-gray-400 hover:text-white transition-colors duration-300 no-underline">
-                        Events
+                    <a href="/#about" className="block text-2xl text-white no-underline font-bold">
+                        ABOUT
                     </a>
-                    <a href="#organizers" className="block text-2xl text-gray-400 hover:text-white transition-colors duration-300 no-underline">
-                        Organizers
+                    <a href="/#shades" className="block text-2xl text-white no-underline font-bold">
+                        ARTISTS
                     </a>
-                    <a href="#sponsors" className="block text-2xl text-gray-400 hover:text-white transition-colors duration-300 no-underline">
-                        Sponsors
+                    <a href="/#events" className="block text-2xl text-white no-underline font-bold">
+                        EVENTS
                     </a>
-                    <a href="#contact" className="block text-2xl text-gray-400 hover:text-white transition-colors duration-300 no-underline">
-                        Contact us
+                    <a href="/teams" className="block text-2xl text-white no-underline font-bold">
+                        TEAM
+                    </a>
+                    <a href="#contact" className="block text-2xl text-white no-underline font-bold">
+                        GET IN TOUCH
                     </a>
                 </div>
             </div>
 
-            {/* Top right icons */}
             <div className="absolute top-6 right-6 z-40">
-                <div className="flex gap-3">
-                    <a 
-                        href="#facebook" 
-                        className="h-8 w-8 border border-black rounded-full flex justify-center items-center hover:bg-black hover:text-white transition-colors cursor-pointer"
+                <div className="flex gap-2 sm:gap-3">
+                    <a
+                        href="https://www.facebook.com/share/1Y8XqRSg9P/"
+                        className="cursor-pointer hover:scale-120 trasition-all duration-300"
                         aria-label="Facebook"
+                        target='_blank'
                     >
-                        <FaFacebookF className="w-4 h-4" />
+                        <img src={facebook} alt="facebook" className='w-12 h-12 sm:h-15 sm:w-15' />
                     </a>
-                    <a 
-                        href="#instagram" 
-                        className="h-8 w-8 border border-black rounded-full flex justify-center items-center hover:bg-black hover:text-white transition-colors cursor-pointer"
-                        aria-label="Instagram"
+                    <a
+                        href="https://www.instagram.com/tbit_impulse?igsh=MWR2OTljcGwyYnk3cA=="
+                        className="cursor-pointer hover:scale-120 trasition-all duration-300"
+                        target='_blank'
                     >
-                        <FaInstagram className="w-4 h-4" />
+                        <img src={instagram} alt="instagram" className='w-12 h-12 sm:h-15 sm:w-15' />
                     </a>
-                    <a 
-                        href="#youtube" 
-                        className="h-8 w-8 border border-black rounded-full flex justify-center items-center hover:bg-black hover:text-white transition-colors cursor-pointer"
-                        aria-label="YouTube"
+                    <a
+                        href="#youtube"
+                        className="cursor-pointer hover:scale-120 trasition-all duration-300"
+                        aria-label="Facebook"
+                        target='_blank'
                     >
-                        <FaYoutube className="w-4 h-4" />
+                        <img src={youtube} alt="instagram" className='w-12 h-12 sm:h-15 sm:w-15' />
                     </a>
-                    <button 
-                        className="h-8 w-8 border border-black rounded-full flex justify-center items-center hover:bg-black hover:text-white transition-colors cursor-pointer"
-                        onClick={openNav}
-                        aria-label="Open navigation"
-                    >
-                        <IoIosMenu className="w-6 h-6" />
-                    </button>
                 </div>
             </div>
         </nav>
